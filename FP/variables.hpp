6a62684d3a69242863e5751f62b22746bf3cf317
Theotrgl <Justintheofilus@gmail.com>
2022-06-15 19:41:36 +0700

    update

--- FP/variables.hpp
+++ FP/variables.hpp
@@ -1,3 +1,4 @@
+//Importing Libraries
 #include <iostream>
 #include <array>
 #include <string>
@@ -5,6 +6,7 @@
 #include <algorithm>
 using namespace std;
 
+//Initializing Variables and Arrays
 int attempts = 6;
 bool win = false;
 bool run = true;
@@ -13,8 +15,10 @@ string AnsKey[] =  {"FROTH", "FLUID", "MONKE","GOOSE","ATONE","MOUTH","PLAIN"};
 array<int,7> s= {0,1,2,3,4,5,6};
 unsigned seed = 1;
 
+//Function to Randomize Answer Key
 string selectAnsKey(){
         shuffle(s.begin(),s.end(),default_random_engine(seed));
-        return AnsKey[s[0]];
+        return AnsKey[s[4]];
 }
+//Assigning Result of Randomization to a Variable
 string key = selectAnsKey();
